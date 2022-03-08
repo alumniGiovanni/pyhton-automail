97e9329cbb30b13092a418b6c3b9ff74a8f065ca
Giovanni <giovanni.andrade9991@gmail.com>
2022-03-07 23:34:24 -0300

    Added window measurement and the writing of the email

--- autoporn.py
+++ autoporn.py
@@ -9,22 +9,17 @@ pag.write("chrome")
 pag.press("enter")
 
 
-pag.hotkey("ctrl", "t")
-pyperclip.copy("https://mail.google.com/mail/u/0/#inbox")
+pag.hotkey("ctrl","shift", "n")
+pyperclip.copy("https://xvideos.com")
 pag.hotkey("ctrl", "v")
 pag.press("enter")
 time.sleep(5)
 
-pag.press("c")
 
-pag.write("giovanni.andrade9991@gmail.com")
-pag.press("tab")
-pag.press("tab")
-pag.write("Email 1")
-pag.press("tab")
-pag.write("Email de teste")
+pag.write("ruiva gostosa")
 pag.press("tab")
 pag.press("enter")
+
 
 
 
